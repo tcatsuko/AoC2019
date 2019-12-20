c3951e37fbe553a2b4314509c2f4825455091145
Richard Morency <brox.za@gmail.com>
2019-12-19 22:32:10 -0600

    Day 18: Many-Worlds Interpretation

--- aoc18.py
+++ aoc18.py
@@ -1,16 +1,18 @@
 import networkx as nx
 import string
 from copy import deepcopy
+from collections import deque
 
-f = open('aoc18.txt','r')
+f = open('aoc18_2.txt','r')
+single_robot = False
+
 raw_input = []
 for line in f:
     raw_input += [line[:-1]]
 f.close()
 clean_graph = nx.Graph()
-edited_graph = nx.Graph()
 # Build graph (x,y) tuples
-start_position = (0,0)
+start_positions = [(0,0),(0,0),(0,0),(0,0)]
 key_positions = {}
 door_positions = {}
 
@@ -34,7 +36,8 @@ def travel_to_nodes(raw_input, start_pos):
                 dirty_move += [item]
     return[clean_move, dirty_move]
 
-    
+
+
 for y in range(1,len(raw_input)-1):
     for x in range(1, len(raw_input[0])-1):
         #Determine type of tile
@@ -43,7 +46,17 @@ for y in range(1,len(raw_input)-1):
             # It's a wall.
             continue
         elif raw_input[y][x] == '@':
-            start_position = (x,y)
+            start_positions[0] = (x,y)
+            single_robot = True
+        elif raw_input[y][x] == '1':
+            start_positions[0] = (x,y)
+            single_robot = False
+        elif raw_input[y][x] == '2':
+            start_positions[1] = (x,y)
+        elif raw_input[y][x] == '3':
+            start_positions[2] = (x,y)
+        elif raw_input[y][x] == '4':
+            start_positions[3] = (x,y)
         # we know it's possible to move to this space
         if current_tile in list(string.ascii_uppercase):
             door_positions[current_tile] = (x,y)
@@ -52,8 +65,7 @@ for y in range(1,len(raw_input)-1):
         clean, dirty = travel_to_nodes(raw_input, (x,y))
         for item in clean:
             clean_graph.add_edge(item, (x,y))
-        for item in dirty:
-            edited_graph.add_edge(item, (x,y))
+
 
 # Build a cached list of routes between each key
 def find_doors_on_route(path, raw_input):
@@ -64,13 +76,26 @@ def find_doors_on_route(path, raw_input):
         if raw_input[y][x] in string.ascii_uppercase:
             doors += raw_input[y][x]
     return doors
-            
+def find_keys_on_route(path, raw_input):
+    keys = []
+    new_path = path[:]
+    del new_path[0]
+    del new_path[-1]
+    for node in path:
+        x = node[0]
+        y = node[1]
+        if raw_input[y][x] in string.ascii_lowercase:
+            keys += raw_input[y][x]
+    return keys
+
 key_routes = {}
 for key in key_positions:
     routes = {}
     for other_key in key_positions:
         short_route = {}
         if other_key == key:
+            continue
+        if not nx.has_path(clean_graph, key_positions[key], key_positions[other_key]):
             continue
         path = nx.shortest_path(clean_graph, key_positions[key], key_positions[other_key])
         doors = find_doors_on_route(path, raw_input)
@@ -80,133 +105,109 @@ for key in key_positions:
     key_routes[key] = routes
 
 # Now do BFS starting at start position.  Assume you can get at least one key without going through a door
-def search(keys_found, position, distance):
-    global key_positions
-    global raw_data
-    global clean_graph
-    global distances
-    global shortest_distance
-    valid_paths = {}
-    for key in key_positions:
-        if key in keys_found:
-            continue
-        path = nx.shortest_path(clean_graph, position, key_positions[key])
-        doors = find_doors_on_route(path, raw_input)
-        doors_copy = doors[:]
-        for door in doors:
-            if string.lower(door) in keys_found:
-                doors_copy.remove(door)
-        if len(doors_copy) == 0:
-            valid_paths[key] = path
-    if len(valid_paths) == 0 and len(keys_found) == len(key_positions):
-        if distance == 62:
-            print()
-        if distance < shortest_distance:
-            shortest_distance = distance
-        return
-    for path in valid_paths:
-        new_position = key_positions[path]
-        new_keys_found = keys_found[:]
-        new_keys_found += path
-        new_distance = distance + len(valid_paths[path]) - 1
-        search(new_keys_found, new_position, new_distance)
+
 lookup_table = {}
 for key in key_positions:
-    new_dictionary = {}
-    for second_key in key_positions:
-        if second_key == key:
-            continue
-        new_dictionary[second_key] = {}
-    lookup_table[key] = new_dictionary
+    lookup_table[key] = {}
 
 all_keys = key_positions.keys()
 all_keys.sort()
 first_key = '@'
-lookup_table['@'] = {}
-for key in all_keys:
-    lookup_table['@'][key] = {}
-    path = nx.shortest_path(clean_graph,start_position, key_positions[key])
-    doors = find_doors_on_route(path, raw_input)
-    path_length = len(path) - 1
-    lookup_table['@'][key]['distance'] = path_length
-    doors_set = set()
-    for item in doors:
-        doors_set.add(item.lower())
-    lookup_table['@'][key]['doors'] = doors_set
+
+if single_robot == True:
+    loops = 1
+    node_names = ['@']
+else:
+    loops = 4
+    node_names = ['1','2','3','4']
+for name in node_names:
+    lookup_table[name] = {}
+for i in range(loops): 
+    start_node = node_names[i]
+    start_position = start_positions[i]
+    for key in all_keys:
+        
+        if not nx.has_path(clean_graph, start_position, key_positions[key]):
+            continue
+        lookup_table[start_node][key] = {}
+        path = nx.shortest_path(clean_graph,start_position, key_positions[key])
+        doors = find_doors_on_route(path, raw_input)
+        keys = find_keys_on_route(path, raw_input)
+        keystring = ''.join(keys)
+        keystring = keystring.replace(key, '')
+        keys_set = set()
+        for item in keys:
+            keys_set.add(item)
+        path_length = len(path) - 1
+        lookup_table[start_node][key] = (path_length, ''.join(doors) + keystring)
+
+
     
-while all_keys:
-    first_key = all_keys[0]
+for first_key in all_keys:
     for key in all_keys:
         if key == first_key:
             continue
+        if not nx.has_path(clean_graph, key_positions[first_key], key_positions[key]):
+            continue
         path = nx.shortest_path(clean_graph, key_positions[first_key], key_positions[key])
         doors = find_doors_on_route(path, raw_input)
+        keys = find_keys_on_route(path, raw_input)
         path_length = len(path) - 1
-        lookup_table[first_key][key]['distance'] = path_length
-        lookup_table[key][first_key]['distance'] = path_length
+        
         doors_set = set()
         for item in doors:
-            doors_set.add(item.lower())
-        lookup_table[first_key][key]['doors'] = doors_set
-        lookup_table[key][first_key]['doors'] = doors_set
-    del all_keys[0]
+            doors_set.add(item)
+        keys_set = set()
+        for item in keys:
+            keys_set.add(item)
+        keystring = ''.join(keys)
+        keystring = keystring.replace(key, '')
+        lookup_table[first_key][key] = (path_length, ''.join(doors) + keystring)
 
 
-distances = []
-shortest_distance = 999999999999999999999999999999999999999999999999999999
-def filter_paths(paths, keyset):
-    data_to_return = {}
+if single_robot == True:
+    info = {(('@'), frozenset()):0}
+else:
+    info = {(('1','2','3','4'),frozenset()):0}
+keys = ''.join(lookup_table.keys())
+keys = keys.replace('@','')
+
+for _ in range(len(lookup_table) - loops):
+    new_info = {}
+    for item in info:
+        location = item[0]
+        found_keys = item[1]
+        current_distance = info[item]
+        for next_key in keys:
+            if next_key not in found_keys:
+                for robot in range(loops):
+                    test1 = location[robot]
+                    if next_key in lookup_table[location[robot]]:
+                        lookup_value = lookup_table[location[robot]][next_key]
+                        distance = lookup_value[0]
+                        on_route = lookup_value[1]
+                        #distance, on_route = lookup_table[location[robot]][next_key]
+                        is_reachable = all((c in found_keys or c.lower() in found_keys) for c in on_route)
+                        
+                        if is_reachable:
+                            new_distance = current_distance + distance
+                            new_keys = frozenset(found_keys | set((next_key,)))
+                            
+                            next_keys = list(location)
+                            next_keys[robot] = next_key
+                            next_keys = tuple(next_keys)
+                            if (next_keys, new_keys) not in  new_info or new_distance < new_info[(next_keys, new_keys)]:
+                                new_info[(next_keys, new_keys)] = new_distance
+    info = new_info
+print('shortest route is ' + str(min(info.values())))
     
-    
-    for k,v in paths.items():
-        if v['doors'].issubset(keyset):
-            data_to_return[k] = v
-    return data_to_return
+
             
+    
 
-def lookup_search(keys_found, position, distance):
-    global key_positions
-    global raw_data
-    global clean_graph
-    global distances
-    global shortest_distance
-    global lookup_table
-    # First find where we can go based on current key
-    # Also determine if we are at the start
-    current_lookup_branch = lookup_table[position]
-    current_keys_found = keys_found.copy()
-    if position != '@':
-        current_keys_found.add(position)
-    possible_paths = 0
-    next_position_keys = []
-    for item in current_lookup_branch.keys():
-        if item in current_keys_found:
-            continue
-        doors = current_lookup_branch[item]['doors']
-        if current_lookup_branch[item]['doors'].issubset(current_keys_found):
-            next_position_keys += item
         
-    for item in keys_found:
-        if item in next_position_keys:
-            next_position_keys.remove(item)
-    if len(next_position_keys) == 0:
-        if distance < shortest_distance:
-            shortest_distance = distance
-        return
-    for item in next_position_keys:
-        lookup_search(current_keys_found, item, distance + current_lookup_branch[item]['distance'])
         
         
     
 
-        
-            
-keys_found = []
-position = start_position
-distance = 0
-
-lookup_search(set(), '@', distance)
-#search(keys_found, start_position, distance)
-#minimum_steps = min(distances)
-print('Part 1: minimum of '+ str(shortest_distance))
             
